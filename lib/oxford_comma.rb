649b3b31d382b85b90df83552b2f5a96b57d4a11
Oscar Zapata <0northbridge.cpu1@gmail.com>
2020-01-27 13:25:01 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,5 @@
 def oxford_comma(array)
-phrase = array.join(" ")
+
+if array.length == 2 # checks to see if array has 2 elements
+"#{array[0]} and #{array[1}" # if so then they interpolate the elements in to a returned string that has the "and" inserted into it 
 end
