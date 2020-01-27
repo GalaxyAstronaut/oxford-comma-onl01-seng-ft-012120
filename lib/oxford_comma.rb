a9be3ae516847b44abdca24521581cd6ecba966e
Oscar Zapata <0northbridge.cpu1@gmail.com>
2020-01-27 13:25:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,4 +2,6 @@ def oxford_comma(array)
 
 if array.length == 2 # checks to see if array has 2 elements
 "#{array[0]} and #{array[1}" # if so then they interpolate the elements in to a returned string that has the "and" inserted into it 
+elsif 2 < array.length # if length of array is greater than 2 
+array[-1].insert
 end
