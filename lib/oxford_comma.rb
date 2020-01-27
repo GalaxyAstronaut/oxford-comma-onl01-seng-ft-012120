d3e43213d1b1c0c0997bf113783218142c99e80c
Oscar Zapata <0northbridge.cpu1@gmail.com>
2020-01-27 13:30:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,5 +5,5 @@ if array.length == 2 # checks to see if array has 2 elements
 elsif 2 < array.length # if length of array is greater than 2 
 array[-1].insert(0, "and") # insert method used which takes two parameters, index and string being passed , then right before last element in array (-1) they put in front of it a "and" ie (0, "and")
 end
-array.join(",")
+array.join(",") # turn array into string 
 end
